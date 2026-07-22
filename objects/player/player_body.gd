7ac6c7f0308c12975f0a96b81a8c8cad0ed9c8e6
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2026-07-22 23:34:53 +0200

    dash

--- objects/player/player_body.gd
+++ objects/player/player_body.gd
@@ -1,19 +1,57 @@
 extends CharacterBody2D
 class_name PlayerBody
 
+@export_category("base_movement")
 @export var speed : float = 150
+@export_category("dash")
 @export var dash : float = 200
-var dir : Vector2 = Vector2.ZERO
+@export var dash_duration : float = 0.4
+@export var dash_cooldown : float = 1
+@export var dash_modifier : float = 2.5
 
+
+var dir : Vector2 = Vector2.ZERO
+var state : State
+
+var _dash_time : float = 0
+var _dash_cooldown : float = 0
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("dash"):
+		_dash()
 
 func _dash() -> void:
-	pass
+	if _dash_cooldown <= 0 && !dir.is_zero_approx():
+		_dash_time = dash_duration
+		_dash_cooldown = dash_cooldown
+
+func handle_state() -> void:
+	if _dash_time > 0:
+		state = State.Dash
+	elif dir.is_zero_approx():
+		state = State.Idle
+	else:
+		state = State.Walking
+
 
 func _physics_process(_delta: float) -> void:
-	dir = Vector2.ZERO
 
-	dir.x = Input.get_axis("left","right")
-	dir.y = Input.get_axis("up","down")
+	_dash_time -= _delta
+	_dash_cooldown -= _delta
 
-	velocity = dir.normalized()*speed
+	if state != State.Dash:
+		dir = Vector2.ZERO
+		dir.x = Input.get_axis("left","right")
+		dir.y = Input.get_axis("up","down")
+		velocity = dir.normalized()*speed
+	else:
+		velocity = dir*dash_modifier*speed
+
+	handle_state()
 	move_and_slide()
+
+enum State{
+	Idle,
+	Walking,
+	Dash
+}
