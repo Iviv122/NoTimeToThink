71378d1eb413ca87bc1497c26bcdcb3a4b03b23f
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2026-07-23 20:21:06 +0200

    sword

---
+++ objects/gun/recocheteGun.gd
@@ -0,0 +1,5 @@
+extends Gun
+class_name Sword
+
+func shoot(init_pos : Vector2,end_pos : Vector2,tree : SceneTree) -> void:
+	pass
--- objects/projectile/bullet.gd
+++ objects/projectile/bullet.gd
@@ -1,12 +1,16 @@
 extends Projectile
 class_name Bullet
 
+@export var destroy_on_hit : bool = true
+
 func hit(body : Node2D) -> void:
 	if body is Enemy:
 		body.damage(parent_gun.damage)
 	elif body is PlayerBody:
 		return;
-	queue_free()
+
+	if destroy_on_hit:
+		queue_free()
 
 # nothing
 func timer_end() -> void:
